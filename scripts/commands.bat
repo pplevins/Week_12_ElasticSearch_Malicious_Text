307c07f5efdeb7d1c497e34e3aff483fa717c250
Pinni Plevinski <shareytora007@gmail.com>
2025-09-02 00:17:02 +0300

    Containerization finished v2

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -1,6 +1,9 @@
 @REM Setting the ElasticSearch container for local run
 docker run -d --name es -p 9200:9200 -e "discovery.type=single-node" -e "xpack.security.enabled=false" -e "ES_JAVA_OPTS=-Xms1g -Xmx1g" docker.elastic.co/elasticsearch/elasticsearch:8.15.0
 
+@Setting the requirements
+pip freeze > requirements.txt
+
 @REM Setting the docker containers
 cd C:\Users\share\PycharmProjects\kodkode\ElasticSearch_Malicious_Text
 docker compose -f compose.yaml up -d
