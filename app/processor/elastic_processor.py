feedecb4854495330938af8c24ad0bcf6ca0d048
Pinni Plevinski <shareytora007@gmail.com>
2025-09-01 17:39:32 +0300

    Basic index creating

--- app/processor/elastic_processor.py
+++ app/processor/elastic_processor.py
@@ -1,15 +1,52 @@
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+
+from app.loader import DataLoader
 
 
 class ElasticSearchProcessor:
     def __init__(self):
-        self.es = Elasticsearch('localhost:9200')
+        self.es = Elasticsearch('http://localhost:9200')
+        self._set_mapping()
 
     def _set_mapping(self):
-        pass
+        mappings = {
+            "properties": {
+                "TweetID": {
+                    "type": "long"
+                },
+                "CreateDate": {
+                    "type": "date"
+                },
+                "Antisemitic": {
+                    "type": "boolean"
+                },
+                "text": {
+                    "type": "text"
+                },
+                "sentiment": {
+                    "type": "keyword"
+                },
+                "weapons": {
+                    "type": "keyword"
+                }
+            }
+        }
+        self.es.indices.delete(index='tweets', ignore_unavailable=True)
+        self.es.indices.create(index='tweets', ignore=400, body=mappings)
+
+    def _generate_documents(self, tweets):
+        for i, doc_data in enumerate(tweets):
+            yield {
+                "_index": "tweets",
+                "_id": i + 1,
+                "_source": doc_data
+            }
 
     def _load_to_es(self):
-        pass
+        tweets = DataLoader.load_from_csv()
+        success, failed = bulk(self.es, self._generate_documents(tweets))
+        print(f"Successfully indexed {success} documents, failed to index {len(failed)} documents.")
 
     def _update_sentiment(self):
         pass
@@ -21,7 +58,7 @@ class ElasticSearchProcessor:
         pass
 
     def process(self):
-        pass
+        self._load_to_es()
 
     def get_antisemitic_with_weapons(self):
         pass
